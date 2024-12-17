f48fa5c3878e056ba6c19709f96130b98d507a81
LyneQ <lyne.quequin@gmail.com>
2024-12-17 12:00:26 +0100

    config(react) setup rooter and scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from "react-router";
+
+import App from './routes/App';
+import Pokemon from './routes/Pokemon';
+
+import './assets/scss/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path={"/pokemon/:id"} element={<Pokemon/>}/>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
