925685c18f668140b76c143a5ad70a62b597e630
lynhe <lynhedev@gmail.com>
2024-12-23 14:13:29 +0100

    feat(pokemon route) fix vite modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
-  plugins: [commonjs(), react()],
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'esm',
-      },
-    },
-  },
+  plugins: [ react()],
 });
