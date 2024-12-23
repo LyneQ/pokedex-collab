96edc18bc35558c9bf3105e7b3c8ad18518efd7b
josias <josiasmbogle237@gmail.com>
2024-12-23 13:18:55 +0100

    test final

--- src/components/ModalPokemon.tsx
+++ src/components/ModalPokemon.tsx
@@ -1,4 +1,4 @@
-import {useEffect,useState} from 'react'
+import {useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom';
 import PokemonType from './PokemonType'
 import SvgComponent from './SvgComponent'
@@ -9,10 +9,11 @@ export default function ModalPokemon({url, closeModal}: {url: string, closeModal
     const [versionPkemon, setVersionPokemon] = useState<Array<{type: string, img: string}>>([])
     const [versionIndex, setVersionIndex] = useState<number>(0)
     const navigate = useNavigate();
-    console.log("pokemon.url",url)
     const urlpokemon = url
+    console.log("urlpokemon - 1",urlpokemon)
     useEffect(() => {
-        fetch(urlpokemon.toString())
+        console.log("urlpokemon - 2",urlpokemon)
+        fetch(urlpokemon)
         .then(response => response.json())
         .then(data => {
             setPokemon({
@@ -28,7 +29,7 @@ export default function ModalPokemon({url, closeModal}: {url: string, closeModal
                 types: data.types,
                 id: data.id
             });
-            console.log(pokemon)
+            console.log("pokmo",pokemon)
             setVersionPokemon([
                 {
                     type: 'normal',
@@ -39,12 +40,12 @@ export default function ModalPokemon({url, closeModal}: {url: string, closeModal
                     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${data.id}.png`
                 }
             ])
-            console.log(data)
+            
         })
         .catch(error => {
             console.error('Error fetching the Pokémon data:', error);
         });
-    }, [url])
+    }, [])
     
     function toggleimage(index: number) {
         setVersionIndex((prevIndex) => (prevIndex + index + versionPkemon.length) % versionPkemon.length)
@@ -61,34 +62,38 @@ export default function ModalPokemon({url, closeModal}: {url: string, closeModal
         <div  onClick={closeModal} className='modalPokemon'>
             <div onClick={(e) => e.stopPropagation()}
              className="pokemonCenter pokemon-front">
-                <SvgComponent types={pokemon!.types}/>
+                {pokemon && <SvgComponent types={pokemon.types}/>}
                 <div className="hp">
                     <p className='nameHp'>HP : </p>
-                    <p className='valueHp'>{pokemon!.stats?.[0]?.base_stat}</p>
+                    <p className='valueHp'>{pokemon?.stats?.[0]?.base_stat}</p>
                 </div>
                 <div onClick = {() => toggleimage(1)} 
                  className="pokemonCenterCard">
                     {currentVersion && (
                         <>
                             <p className='name'><span>{currentVersion.type}</span> </p>
-                            <img src={currentVersion.img} alt={pokemon!.name} />
+                            <img src={currentVersion.img} alt={pokemon?.name} />
                         </>
                     )}
-                    <h3>{pokemon!.name}</h3>
-                    <PokemonType type={pokemon!.types.toString()} />
+                    <h3>{pokemon?.name}</h3>
+                    <div className={"pokemonType"}>
+                    { pokemon?.types?.map(( data: any, index: number) => {
+                        return <PokemonType type={data.type.name} key={index} />
+                    })}
+                </div>
                     
                 </div>
                 <div className="stats">
                     <div className="attack">
-                        <p>{pokemon!.stats?.[1]?.base_stat}</p>
+                        <p>{pokemon?.stats?.[1]?.base_stat}</p>
                         <h4>Attack</h4>
                     </div>
                     <div className="defense">
-                        <p>{pokemon!.stats?.[2]?.base_stat}</p>
+                        <p>{pokemon?.stats?.[2]?.base_stat}</p>
                         <h4>Defense</h4>
                     </div>
                     <div className="speed">
-                        <p>{pokemon!.stats?.[5]?.base_stat}</p>
+                        <p>{pokemon?.stats?.[5]?.base_stat}</p>
                         <h4>Speed</h4>
                     </div>
                 </div>
