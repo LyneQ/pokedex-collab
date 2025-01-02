01d6331774e5783f5c7a4295a2589a06d52d9f00
lynhe <lynhedev@gmail.com>
2025-01-02 16:32:27 +0100

    build(build) remove base rules from vites config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: "/pokedex-collab/",
   build: {
     rollupOptions: {
       external: ['module-needing-require'], // Skip bundling problematic modules
